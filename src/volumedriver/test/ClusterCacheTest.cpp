bf04a69db21ecd876abb3fb72ca767fc9a69a20d
Chrysostomos Nanakos <cnanakos@openvstorage.com>
2015-10-23 18:41:53 +0300

    Add missing license header
    Signed-off-by: Chrysostomos Nanakos <cnanakos@openvstorage.com>

--- src/volumedriver/test/ClusterCacheTest.cpp
+++ src/volumedriver/test/ClusterCacheTest.cpp
@@ -11,10 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
-//
-// contains excerpts from "The Lyre Of Orpheus", by Nick Cave.
-//
+
 #include "VolManagerTestSetup.h"
 
 #include <errno.h>
