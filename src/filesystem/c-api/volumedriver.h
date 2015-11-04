c6f3889827ca592a20a39c74f69def1bab6f8bcc
Chrysostomos Nanakos <cnanakos@openvstorage.com>
2015-11-04 11:46:35 +0200

    c-api: Introduce libovsvolumedriver declarations
    Signed-off-by: Chrysostomos Nanakos <cnanakos@openvstorage.com>

---
+++ src/filesystem/c-api/volumedriver.h
@@ -0,0 +1,334 @@
+// Copyright 2015 iNuron NV
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef __LIB_VOLUMEDRIVER_H_
+#define __LIB_VOLUMEDRIVER_H_
+
+#include <pthread.h>
+#include <sys/types.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+typedef struct _ovs_buffer
+{
+    void *buf;
+    size_t size;
+} ovs_buffer_t;
+
+typedef struct _ovs_iothread
+{
+    pthread_t io_t;
+    pthread_mutex_t io_mutex;
+    pthread_cond_t io_cond;
+    bool stopped;
+    bool stopping;
+} ovs_iothread_t;
+
+typedef struct _ovs_ctx_t
+{
+    void *shm_handle_;
+    int oflag;
+    /* Asynchronous I/O members */
+    ovs_iothread_t *rr_iothread;
+    ovs_iothread_t *wr_iothread;
+} ovs_ctx_t;
+
+typedef struct _ovs_aio_request ovs_aio_request;
+
+struct ovs_aiocb
+{
+    void *aio_buf;
+    off_t aio_offset;
+    ssize_t aio_nbytes;
+    /* Internal members */
+    bool _on_suspend;
+    bool _canceled;
+    bool _completed;
+    bool _signaled;
+    int _op;
+    int _errno;
+    ssize_t _rv;
+    ovs_aio_request *_request;
+    pthread_cond_t _cond;
+    pthread_mutex_t _mutex;
+};
+
+typedef struct _ovs_completion_t ovs_completion_t;
+
+typedef void (*ovs_callback_t)(ovs_completion_t *cb, void *arg);
+
+struct _ovs_completion_t
+{
+    ovs_callback_t complete_cb;
+    void *cb_arg;
+    /* Internal members */
+    bool _on_wait;
+    bool _calling;
+    bool _signaled;
+    ssize_t _rv;
+    pthread_cond_t _cond;
+    pthread_mutex_t _mutex;
+};
+
+struct _ovs_aio_request
+{
+    struct ovs_aiocb *ovs_aiocbp;
+    ovs_completion_t *completion;
+};
+
+/*
+ * Initialize Open vStorage context
+ * param volume_name: Volume name
+ * param oflag: Open flags
+ * return: Open vStorage context on success, or NULL on fail
+ */
+ovs_ctx_t*
+ovs_ctx_init(const char *volume_name,
+             int oflag);
+
+/*
+ * Destroy Open vStorage context
+ * param: Open vStorage context
+ * return: 0 on success, -1 on fail
+ */
+int
+ovs_ctx_destroy(ovs_ctx_t **ctx);
+
+/*
+ * Create an Open vStorage volume
+ * param volume_name: Volume name
+ * param size: Size of volume in bytes
+ */
+int
+ovs_create_volume(const char *volume_name,
+                  uint64_t size);
+
+/*
+ * Allocate buffer from the shared memory segment
+ * param ctx: Open vStorage context
+ * param size: Buffer size in bytes
+ * return: Buffer pointer on success, or NULL on fail
+ */
+ovs_buffer_t*
+ovs_allocate(ovs_ctx_t *ctx,
+             size_t size);
+
+/*
+ * Deallocate previously allocated buffer
+ * param ctx: Open vStorage context
+ * param shptr: Buffer pointer
+ * return: 0 on success, -1 on fail
+ */
+int
+ovs_deallocate(ovs_ctx_t *ctx,
+               ovs_buffer_t **ptr);
+
+/*
+ * Read from a volume
+ * param ctx: Open vStorage context
+ * param buf: Shared memory buffer
+ * param nbytes: Size to read in bytes
+ * param offset: Offset to read in volume
+ * return: Number of bytes actually read, -1 on fail
+ */
+ssize_t
+ovs_read(ovs_ctx_t *ctx,
+         void *buf,
+         size_t nbytes,
+         off_t offset);
+
+/*
+ * Write to a volume
+ * param ctx: Open vStorage context
+ * param buf: Shared memory buffer
+ * param nbytes: Size to write in bytes
+ * param offset: Offset to write in volume
+ * return: Number of bytes actually written, -1 on fail
+ */
+ssize_t
+ovs_write(ovs_ctx_t *ctx,
+          const void *buf,
+          size_t nbytes,
+          off_t offset);
+
+/*
+ * Syncronize a volume's in-core state with that on disk
+ * param ctx: Open vStorage context
+ * return: 0 on success, -1 on fail
+ */
+int
+ovs_flush(ovs_ctx_t *ctx);
+
+/*
+ * Get volume status
+ * param ctx: Open vStorage context
+ * param buf: Pointer to a stat structure
+ * return: 0 on success, -1 on fail
+ */
+int
+ovs_stat(ovs_ctx_t *ctx,
+         struct stat *buf);
+
+/*
+ * Suspend until asynchronous I/O operation or timeout complete
+ * param ctx: Open vStorage context
+ * param ovs_aiocb: Pointer to an AIO Control Block structure
+ * param timeout: Pointer to a timespec structure
+ * return: 0 on success, -1 on fail
+ */
+int
+ovs_aio_suspend(ovs_ctx_t *ctx,
+                struct ovs_aiocb *ovs_aiocb,
+                const struct timespec *timeout);
+
+/*
+ * Retrieve error status of asynchronous I/O operation
+ * param ctx: Open vStorage context
+ * param ovs_aiocb: Pointer to an AIO Control Block structure
+ * return: 0 on success, -1 on fail
+ */
+int
+ovs_aio_error(ovs_ctx_t *ctx,
+              struct ovs_aiocb *ovs_aiocbp);
+
+/*
+ * Retrieve return status of asynchronous I/O operation
+ * param ctx: Open vStorage context
+ * param ovs_aiocb: Pointer to an AIO Control Block structure
+ * return: Number of bytes returned based on the operation, -1 on fail
+ */
+ssize_t
+ovs_aio_return(ovs_ctx_t *ctx,
+               const struct ovs_aiocb *ovs_aiocbp);
+
+/*
+ * Cancel an oustanding asynchronous I/O operation
+ * param ctx: Open vStorage context
+ * param ovs_aiocb: Pointer to an AIO Control Block structure
+ * return: 0 on success, -1 on fail
+ */
+int
+ovs_aio_cancel(ovs_ctx_t *ctx,
+               struct ovs_aiocb *ovs_aiocbp);
+
+/*
+ * Asynchronous read from a volume
+ * param ctx: Open vStorage context
+ * param ovs_aiocb: Pointer to an AIO Control Block structure
+ * return: 0 on success, -1 on fail
+ */
+int
+ovs_aio_read(ovs_ctx_t *ctx,
+             struct ovs_aiocb *ovs_aiocbp);
+
+/*
+ * Asynchronous write to a volume
+ * param ctx: Open vStorage context
+ * param ovs_aiocb: Pointer to an AIO Control Block structure
+ * return: 0 on success, -1 on fail
+ */
+int
+ovs_aio_write(ovs_ctx_t *ctx,
+              struct ovs_aiocb *ovs_aiocbp);
+
+/*
+ * Asynchronous read from a volume with completion
+ * param ctx: Open vStorage context
+ * param ovs_aiocb: Pointer to an AIO Control Block structure
+ * param completion: Pointer to a completion structure
+ * return: 0 on success, -1 on fail
+ */
+int
+ovs_aio_readcb(ovs_ctx_t *ctx,
+               struct ovs_aiocb *ovs_aiocbp,
+               ovs_completion_t *completion);
+
+/*
+ * Asynchronous write to a volume with completion
+ * param ctx: Open vStorage context
+ * param ovs_aiocb: Pointer to an AIO Control Block structure
+ * param completion: Pointer to a completion structure
+ * return: 0 on success, -1 on fail
+ */
+int
+ovs_aio_writecb(ovs_ctx_t *ctx,
+                struct ovs_aiocb *ovs_aiocbp,
+                ovs_completion_t *completion);
+
+/*
+ * Asynchronously syncronize a volume's in-core state with that on disk with
+ * completion
+ * param ctx: Open vStorage context
+ * param completion: Pointer to a completion structure
+ * return: 0 on success, -1 on fail
+ */
+int
+ovs_aio_flushcb(ovs_ctx_t *ctx,
+                ovs_completion_t *completion);
+
+/*
+ * Create a new completion
+ * param complete_cb: Pointer to an ovs_callback_t structure
+ * param arg: Pointer to an argument passed to complete_cb
+ * return: Completion pointer on success, or NULL on fail
+ */
+ovs_completion_t*
+ovs_aio_create_completion(ovs_callback_t complete_cb,
+                          void *arg);
+
+/*
+ * Retrieve return status of a completion
+ * param completion: Pointer to a completion structure
+ * return: Number of bytes returned based on the operation and the completion,
+ * -1 on fail
+ */
+ssize_t
+ovs_aio_return_completion(ovs_completion_t *completion);
+
+/*
+ * Suspend until completion or timeout complete
+ * param completion: Pointer to completion structure
+ * param timeout: Pointer to a timespec structure
+ * return: 0 on success, -1 on fail
+ */
+int
+ovs_aio_wait_completion(ovs_completion_t *completion,
+                        const struct timespec *timeout);
+
+/*
+ * Signal a suspended completion
+ * param completion: Pointer to completion structure
+ * return: 0 on success, -1 on fail
+ */
+int
+ovs_aio_signal_completion(ovs_completion_t *completion);
+
+/*
+ * Release completion
+ * param completion: Pointer to completion structure
+ * return: 0 on success, -1 on fail
+ */
+int
+ovs_aio_release_completion(ovs_completion_t **completion);
+
+#ifdef __cplusplus
+} //extern "C" endif
+#endif
+
+#endif // __LIB_OPENVSTORAGE_H_
