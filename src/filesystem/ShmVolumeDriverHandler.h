openvstorage/volumedriver

commit a489fc0afb03e19501b6a51ce90ea404f1d6ab2b

ShmVolumeDriverHandler: use Handles for read/write/flush
This way the SHM interface can also enjoy the benefits of the recently
introduced "fast lane".

Part of OVS-4320.

src/filesystem/ShmVolumeDriverHandler.h
@@ -40,43 +40,51 @@ public:
     ShmVolumeDriverHandler(const ::ShmIdlInterface::CreateShmArguments& args,
                            construction_args& handler_args)
         : fs_(handler_args.fs)
-        , objectid_(*get_objectid(make_volume_path(std::string(args.volume_name))))
-        , volume_size_in_bytes_(get_volume_size(objectid_))
         , shm_segment_(new boost::interprocess::managed_shared_memory(boost::interprocess::open_only,
                                                                       ShmSegmentDetails::Name()))
     {
+        open(std::string(args.volume_name));
+
         LOG_INFO("created a new volume handler for volume '" <<
                 args.volume_name << "'");
     }
 
     explicit ShmVolumeDriverHandler(construction_args& handler_args)
         : fs_(handler_args.fs)
-        , volume_size_in_bytes_(0)
     {
         LOG_INFO("created a new volume handler");
     }
 
-    ~ShmVolumeDriverHandler() = default;
+    ~ShmVolumeDriverHandler()
+    {
+        if (handle_)
+        {
+            fs_.release(std::move(handle_));
+        }
+    }
 
     void
     write(const ShmWriteRequest* request,
           ShmWriteReply* reply)
     {
+        VERIFY(handle_);
+
         LOG_TRACE("write request offset: " << request->offset_in_bytes
                   << ", size: " << request->size_in_bytes);
 
-        uint8_t *data = static_cast<uint8_t*>(shm_segment_->get_address_from_handle(request->handle));
+        auto data = static_cast<const char*>(shm_segment_->get_address_from_handle(request->handle));
 
         reply->opaque = request->opaque;
         reply->size_in_bytes = request->size_in_bytes;
 
+        bool sync = false;
         try
         {
-            fs_.object_router().write(nullptr,
-                                      objectid_,
-                                      data,
-                                      reply->size_in_bytes,
-                                      request->offset_in_bytes);
+            fs_.write(*handle_,
+                      reply->size_in_bytes,
+                      data,
+                      request->offset_in_bytes,
+                      sync);
             reply->failed = false;
         }
         CATCH_STD_ALL_EWHAT({
@@ -88,38 +96,44 @@ public:
     bool
     flush()
     {
+        VERIFY(handle_);
+
         LOG_TRACE("Flushing");
+
         try
         {
-            fs_.object_router().sync(nullptr,
-                                     objectid_);
+            fs_.fsync(*handle_,
+                      false);
+            return true;
         }
         CATCH_STD_ALL_EWHAT({
             LOG_ERROR("flush I/O error: " << EWHAT);
             return false;
         });
-        return true;
     }
 
     void
     read(const ShmReadRequest* request,
          ShmReadReply* reply)
     {
+        VERIFY(handle_);
+
         LOG_TRACE("read request offset: " << request->offset_in_bytes
                   << ", size_in_bytes: " << request->size_in_bytes);
 
         reply->opaque = request->opaque;
         reply->size_in_bytes = request->size_in_bytes;
 
-        uint8_t *data = static_cast<uint8_t*>(shm_segment_->get_address_from_handle(request->handle));
+        auto data = static_cast<char*>(shm_segment_->get_address_from_handle(request->handle));
 
         try
         {
-            fs_.object_router().read(nullptr,
-                                     objectid_,
-                                     data,
-                                     reply->size_in_bytes,
-                                     request->offset_in_bytes);
+            bool eof = false;
+            fs_.read(*handle_,
+                     reply->size_in_bytes,
+                     data,
+                     request->offset_in_bytes,
+                     eof);
             reply->failed = false;
         }
         CATCH_STD_ALL_EWHAT({
@@ -131,7 +145,8 @@ public:
     uint64_t
     volume_size_in_bytes() const
     {
-        return volume_size_in_bytes_;
+        VERIFY(handle_);
+        return fs_.object_router().get_size(handle_->dentry()->object_id());
     }
 
     void
@@ -140,6 +155,8 @@ public:
     {
         LOG_INFO("Create new volume with name: " << volume_name
                  << ", size: " << volume_size_in_bytes);
+
+        VERIFY(not handle_);
 
         const std::string root_("/");
         const std::string dot_(".");
@@ -163,6 +180,15 @@ public:
                      << ":" << e.what());
             throw;
         }
+
+        try
+        {
+            open(volume_name);
+        }
+        CATCH_STD_ALL_EWHAT({
+                LOG_ERROR("Failed to open " << volume_name << ": " << EWHAT);
+                remove_volume(volume_name);
+            });
     }
 
     void
@@ -391,8 +417,7 @@ private:
     DECLARE_LOGGER("ShmVolumeDriverHandler");
 
     FileSystem& fs_;
-    const ObjectId objectid_;
-    const uint64_t volume_size_in_bytes_;
+    Handle::Ptr handle_;
 
     std::unique_ptr<boost::interprocess::managed_shared_memory> shm_segment_;
 
@@ -419,6 +444,19 @@ private:
         const std::string root_("/");
         return FrontendPath(root_ + volume_name +
                             fs_.vdisk_format().volume_suffix());
+    }
+
+    void
+    open(const std::string& volname)
+    {
+        VERIFY(not handle_);
+
+        const FrontendPath p(make_volume_path(volname));
+        // called for its side effect of checking the object presence
+        get_objectid(p);
+        fs_.open(p,
+                 O_RDWR,
+                 handle_);
     }
 };
 

commit f19f9c7e663f58320f1fbfad7a8148ea4cb50da7

ShmVolumeDriverHandler: rethrow error during failed volume creation
Regression introduced with a489fc0afb03e19501b6a51ce90ea404f1d6ab2b

Part of OVS-4320.

src/filesystem/ShmVolumeDriverHandler.h
@@ -188,6 +188,7 @@ public:
         CATCH_STD_ALL_EWHAT({
                 LOG_ERROR("Failed to open " << volume_name << ": " << EWHAT);
                 remove_volume(volume_name);
+                throw;
             });
     }
 