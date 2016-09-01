70e075a202bd7410dfdae8b3686a2f40484b1d90
Chrysostomos Nanakos <cnanakos@openvstorage.com>
2016-09-01 11:18:42 +0300

    c-api: Factor out volume cache handler
    Signed-off-by: Chrysostomos Nanakos <cnanakos@openvstorage.com>

--- src/filesystem/c-api/VolumeCacheHandler.h
+++ src/filesystem/c-api/VolumeCacheHandler.h
@@ -16,6 +16,8 @@
 #ifndef _VOLUME_CACHE_HANDLER_H
 #define _VOLUME_CACHE_HANDLER_H
 
+#include "volumedriver.h"
+#include "common.h"
 #include "ShmClient.h"
 #include "ShmControlChannelClient.h"
 
@@ -29,146 +31,24 @@ class VolumeCacheHandler
 {
 public:
     VolumeCacheHandler(volumedriverfs::ShmClientPtr shm_client,
-                       ShmControlChannelClientPtr ctl_client)
-    : shm_client_(shm_client)
-    , ctl_client_(ctl_client)
-    {}
+                       ShmControlChannelClientPtr ctl_client);
 
-    ~VolumeCacheHandler()
-    {
-        drop_caches();
-    };
+    ~VolumeCacheHandler();
 
     VolumeCacheHandler(const VolumeCacheHandler&) = delete;
     VolumeCacheHandler& operator=(const VolumeCacheHandler&) = delete;
 
     ovs_buffer_t*
-    allocate(size_t size)
-    {
-        ovs_buffer_t *buffer;
-        int retries = 15;
-        boost::posix_time::time_duration delay =
-            boost::posix_time::nanoseconds(10);
-        while ((buffer = _try_allocate(size)) == NULL)
-        {
-            boost::this_thread::sleep(delay);
-            retries--;
-            delay *= 2;
-            if (retries == 0)
-            {
-                break;
-            }
-        }
-        return buffer;
-    }
+    allocate(size_t size);
 
     int
-    deallocate(ovs_buffer_t *shptr)
-    {
-        if (shptr)
-        {
-            switch (shptr->size)
-            {
-            case BufferSize::s_4k:
-                return _maybe_deallocate_to_queue(shptr,
-                                                  chunks_4k,
-                                                  lock_4k,
-                                                  QueueSize::qs_4k);
-            case BufferSize::s_32k:
-                return _maybe_deallocate_to_queue(shptr,
-                                                  chunks_32k,
-                                                  lock_32k,
-                                                  QueueSize::qs_32k);
-            case BufferSize::s_128k:
-                return _maybe_deallocate_to_queue(shptr,
-                                                  chunks_128k,
-                                                  lock_128k,
-                                                  QueueSize::qs_128k);
-            default:
-                void *ptr = shptr->buf;
-                delete shptr;
-                return ctl_channel_deallocate(ptr);
-            }
-        }
-        else
-        {
-            return -1;
-        }
-    }
+    deallocate(ovs_buffer_t *shptr);
 
     int
-    preallocate()
-    {
-        for (int i = 0; i < QueueSize::qs_4k; i++)
-        {
-            void *ptr = ctl_channel_allocate(BufferSize::s_4k);
-            if (ptr)
-            {
-                chunks_4k.push(ptr);
-            }
-            else
-            {
-                return -1;
-            }
-        }
-        for (int i = 0; i < QueueSize::qs_32k; i++)
-        {
-            void *ptr = ctl_channel_allocate(BufferSize::s_32k);
-            if (ptr)
-            {
-                chunks_32k.push(ptr);
-            }
-            else
-            {
-                return -1;
-            }
-        }
-        for (int i = 0; i < QueueSize::qs_128k; i++)
-        {
-            void *ptr = ctl_channel_allocate(BufferSize::s_128k);
-            if (ptr)
-            {
-                chunks_128k.push(ptr);
-            }
-            else
-            {
-                return -1;
-            }
-        }
-        return 0;
-    }
+    preallocate();
 
     void
-    drop_caches()
-    {
-        {
-            fungi::ScopedSpinLock lock_(lock_4k);
-            while (not chunks_4k.empty())
-            {
-                void *buf = chunks_4k.front();
-                chunks_4k.pop();
-                ctl_channel_deallocate(buf);
-            }
-        }
-        {
-            fungi::ScopedSpinLock lock_(lock_32k);
-            while (not chunks_32k.empty())
-            {
-                void *buf = chunks_32k.front();
-                chunks_32k.pop();
-                ctl_channel_deallocate(buf);
-            }
-        }
-        {
-            fungi::ScopedSpinLock lock_(lock_128k);
-            while (not chunks_128k.empty())
-            {
-                void *buf = chunks_128k.front();
-                chunks_128k.pop();
-                ctl_channel_deallocate(buf);
-            }
-        }
-    }
+    drop_caches();
 
 private:
     typedef std::queue<void*> BufferQueue;
@@ -197,117 +77,25 @@ private:
     fungi::SpinLock lock_128k;
 
     ovs_buffer_t*
-    _try_allocate(size_t size)
-    {
-        void *buf = NULL;
-        size_t max = BufferSize::s_4k;
-        buf = _maybe_allocate_from_queue(size,
-                                         chunks_4k,
-                                         lock_4k,
-                                         max);
-        if (not buf)
-        {
-            max = BufferSize::s_32k;
-            buf = _maybe_allocate_from_queue(size,
-                                             chunks_32k,
-                                             lock_32k,
-                                             max);
-        }
-        if (not buf)
-        {
-            max = BufferSize::s_128k;
-            buf = _maybe_allocate_from_queue(size,
-                                             chunks_128k,
-                                             lock_128k,
-                                             max);
-        }
-        if (not buf)
-        {
-            max = size;
-            buf = ctl_channel_allocate(size);
-        }
-        if (buf)
-        {
-            ovs_buffer_t *ovs_buff = new ovs_buffer_t;
-            ovs_buff->buf = buf;
-            ovs_buff->size = max;
-            return ovs_buff;
-        }
-        else
-        {
-            return NULL;
-        }
-    }
-
+    _try_allocate(size_t size);
 
     void*
-    ctl_channel_allocate(size_t size)
-    {
-        boost::interprocess::managed_shared_memory::handle_t handle;
-        bool ret = ctl_client_->allocate(handle, size);
-        if (ret)
-        {
-            void *ptr = shm_client_->get_address_from_handle(handle);
-            return ptr;
-        }
-        return NULL;
-    }
+    ctl_channel_allocate(size_t size);
 
     int
-    ctl_channel_deallocate(void *buf)
-    {
-        boost::interprocess::managed_shared_memory::handle_t handle =
-            shm_client_->get_handle_from_address(buf);
-        return (ctl_client_->deallocate(handle) ? 0 : -1);
-    }
+    ctl_channel_deallocate(void *buf);
 
     void*
     _maybe_allocate_from_queue(size_t size,
                                BufferQueue& queue,
                                fungi::SpinLock& lock_,
-                               size_t max)
-    {
-        if (size <= max)
-        {
-            {
-                fungi::ScopedSpinLock queue_lock(lock_);
-                if (not queue.empty())
-                {
-                    void *buf = queue.front();
-                    queue.pop();
-                    return buf;
-                }
-            }
-            return ctl_channel_allocate(max);
-        }
-        else
-        {
-            return NULL;
-        }
-    }
+                               size_t max);
 
     int
     _maybe_deallocate_to_queue(ovs_buffer_t *shptr,
                                BufferQueue& queue,
                                fungi::SpinLock& lock_,
-                               size_t queue_size)
-    {
-        void *ptr = shptr->buf;
-        delete shptr;
-
-        lock_.lock();
-        if (queue.size() < queue_size + 1)
-        {
-            queue.push(ptr);
-            lock_.unlock();
-        }
-        else
-        {
-            lock_.unlock();
-            return ctl_channel_deallocate(ptr);
-        }
-        return 0;
-    }
+                               size_t queue_size);
 };
 
 typedef std::unique_ptr<VolumeCacheHandler> VolumeCacheHandlerPtr;
