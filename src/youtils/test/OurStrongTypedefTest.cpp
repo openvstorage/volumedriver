openvstorage/volumedriver

commit 5f283de3fd0d41943d22219094e6935e783b520f

filesystem/ClusterNodeConfig: introduce network_server_uri member
Part of https://github.com/openvstorage/volumedriver/issues/80

src/filesystem/ClusterNodeConfig.h
@@ -25,6 +25,7 @@
 #include <youtils/Assert.h>
 #include <youtils/OurStrongTypedef.h>
 #include <youtils/Serialization.h>
+#include <youtils/Uri.h>
 
 OUR_STRONG_ARITHMETIC_TYPEDEF(uint16_t, MessagePort, volumedriverfs);
 OUR_STRONG_ARITHMETIC_TYPEDEF(uint16_t, XmlRpcPort, volumedriverfs);
@@ -35,16 +36,20 @@ namespace volumedriverfs
 
 struct ClusterNodeConfig
 {
+    using MaybeUri = boost::optional<youtils::Uri>;
+
     ClusterNodeConfig(const NodeId& id,
                       const std::string& h,
                       MessagePort mport,
                       XmlRpcPort xport,
-                      FailoverCachePort fport)
+                      FailoverCachePort fport,
+                      const MaybeUri& nw_uri)
         : vrouter_id(id)
         , host(h)
         , message_port(mport)
         , xmlrpc_port(xport)
         , failovercache_port(fport)
+        , network_server_uri(nw_uri)
     {
         //we don't allow empty node_id's as the empty string is
         //used in our python API as "don't care"
@@ -59,6 +64,7 @@ struct ClusterNodeConfig
         , message_port(other.message_port)
         , xmlrpc_port(other.xmlrpc_port)
         , failovercache_port(other.failovercache_port)
+        , network_server_uri(other.network_server_uri)
     {}
 
     ClusterNodeConfig&
@@ -71,6 +77,7 @@ struct ClusterNodeConfig
             const_cast<MessagePort&>(message_port) = other.message_port;
             const_cast<XmlRpcPort&>(xmlrpc_port) = other.xmlrpc_port;
             const_cast<FailoverCachePort&>(failovercache_port) = other.failovercache_port;
+            const_cast<MaybeUri&>(network_server_uri) = other.network_server_uri;
         }
 
         return *this;
@@ -84,7 +91,8 @@ struct ClusterNodeConfig
             (host == other.host) and
             (message_port == other.message_port) and
             (xmlrpc_port == other.xmlrpc_port) and
-            (failovercache_port == other.failovercache_port);
+            (failovercache_port == other.failovercache_port) and
+            (network_server_uri == other.network_server_uri);
     }
 
     bool
@@ -93,17 +101,45 @@ struct ClusterNodeConfig
         return not (*this == other);
     }
 
+    BOOST_SERIALIZATION_SPLIT_MEMBER();
+
     template<class Archive>
     void
-    serialize(Archive& ar, const unsigned version)
+    load(Archive& ar, const unsigned version)
     {
-        CHECK_VERSION(version, 1);
+        if (version > 2)
+        {
+            THROW_SERIALIZATION_ERROR(version, 2, 1);
+        }
 
         ar & const_cast<NodeId&>(vrouter_id);
         ar & const_cast<std::string&>(host);
-        ar & static_cast<uint16_t&>(const_cast<MessagePort&>(message_port));
-        ar & static_cast<uint16_t&>(const_cast<XmlRpcPort&>(xmlrpc_port));
-        ar & static_cast<uint16_t&>(const_cast<FailoverCachePort&>(failovercache_port));
+        ar & const_cast<MessagePort&>(message_port);
+        ar & const_cast<XmlRpcPort&>(xmlrpc_port);
+        ar & const_cast<FailoverCachePort&>(failovercache_port);
+
+        if (version > 1)
+        {
+            ar & const_cast<MaybeUri&>(network_server_uri);
+        }
+        else
+        {
+            const_cast<MaybeUri&>(network_server_uri) = boost::none;
+        }
+    }
+
+    template<class Archive>
+    void
+    save(Archive& ar, const unsigned version) const
+    {
+        CHECK_VERSION(version, 2);
+
+        ar & vrouter_id;
+        ar & host;
+        ar & message_port;
+        ar & xmlrpc_port;
+        ar & failovercache_port;
+        ar & network_server_uri;
     }
 
     // for python consumption
@@ -115,6 +151,7 @@ struct ClusterNodeConfig
     const MessagePort message_port;
     const XmlRpcPort xmlrpc_port;
     const FailoverCachePort failovercache_port;
+    const MaybeUri network_server_uri;
 };
 
 std::ostream&
@@ -125,7 +162,7 @@ using ClusterNodeConfigs = std::vector<ClusterNodeConfig>;
 
 }
 
-BOOST_CLASS_VERSION(volumedriverfs::ClusterNodeConfig, 1);
+BOOST_CLASS_VERSION(volumedriverfs::ClusterNodeConfig, 2);
 
 namespace boost
 {
@@ -145,7 +182,8 @@ load_construct_data(Archive& ar,
                                   "",
                                   MessagePort(0),
                                   XmlRpcPort(0),
-                                  FailoverCachePort(0));
+                                  FailoverCachePort(0),
+                                  youtils::Uri());
 }
 
 }

src/filesystem-python-client/StorageRouterClient.cpp
@@ -874,26 +874,38 @@ BOOST_PYTHON_MODULE(storagerouterclient)
     REGISTER_STRONG_ARITHMETIC_TYPEDEF_CONVERTER(vfs::XmlRpcPort);
     REGISTER_STRONG_ARITHMETIC_TYPEDEF_CONVERTER(vfs::FailoverCachePort);
 
-    bpy::class_<vfs::ClusterNodeConfig>("ClusterNodeConfig",
-                                        "configuration of a single volumedriverfs cluster node",
-                                        bpy::init<const vfs::NodeId&,
-                                                  const std::string&,
-                                                  vfs::MessagePort,
-                                                  vfs::XmlRpcPort,
-                                                  vfs::FailoverCachePort>((bpy::args("vrouter_id"),
-                                                                           bpy::args("host"),
-                                                                           bpy::args("message_port"),
-                                                                           bpy::args("xmlrpc_port"),
-                                                                           bpy::args("failovercache_port")),
-                                                                          "Create a cluster node configuration\n"
-                                                                          "@param vrouter_id: string, node ID\n"
-                                                                          "@param host: string, hostname or IP address\n"
-                                                                          "@param message_port: uint16_t, TCP port used for communication between nodes\n"
-                                                                          "@param xmlrpc_port: uint16_t, TCP port used for XMLRPC based management\n"
-                                                                          "@param failovercache_port: uint16_t, TCP port of the FailoverCache for this node\n"))
-        .def("__str__", &vfs::ClusterNodeConfig::str)
-        .def("__repr__", &vfs::ClusterNodeConfig::str)
-        .def("__eq__", &vfs::ClusterNodeConfig::operator==)
+    using MaybeUri = boost::optional<youtils::Uri>;
+
+    REGISTER_OPTIONAL_CONVERTER(youtils::Uri);
+
+    bpy::class_<vfs::ClusterNodeConfig>
+        ("ClusterNodeConfig",
+         "configuration of a single volumedriverfs cluster node",
+         bpy::init<const vfs::NodeId&,
+                   const std::string&,
+                   vfs::MessagePort,
+                   vfs::XmlRpcPort,
+                   vfs::FailoverCachePort,
+                   const MaybeUri&>
+         ((bpy::args("vrouter_id"),
+           bpy::args("host"),
+           bpy::args("message_port"),
+           bpy::args("xmlrpc_port"),
+           bpy::args("failovercache_port"),
+           bpy::args("network_server_uri") = MaybeUri()),
+          "Create a cluster node configuration\n"
+          "@param vrouter_id: string, node ID\n"
+          "@param host: string, hostname or IP address\n"
+          "@param message_port: uint16_t, TCP port used for communication between nodes\n"
+          "@param xmlrpc_port: uint16_t, TCP port used for XMLRPC based management\n"
+          "@param failovercache_port: uint16_t, TCP port of the FailoverCache for this node\n"
+          "@param network_server_uri: optional string (URI), URI the network server shall listen on\n"))
+        .def("__str__",
+             &vfs::ClusterNodeConfig::str)
+        .def("__repr__",
+             &vfs::ClusterNodeConfig::str)
+        .def("__eq__",
+             &vfs::ClusterNodeConfig::operator==)
 
 #define DEF_READONLY_PROP_(name)                                \
         .def_readonly(#name, &vfs::ClusterNodeConfig::name)
@@ -903,6 +915,7 @@ BOOST_PYTHON_MODULE(storagerouterclient)
         DEF_READONLY_PROP_(message_port)
         DEF_READONLY_PROP_(xmlrpc_port)
         DEF_READONLY_PROP_(failovercache_port)
+        DEF_READONLY_PROP_(network_server_uri)
 
 #undef DEF_READONLY_PROP_
         ;

src/filesystem/ClusterNodeStatus.h
@@ -110,7 +110,8 @@ load_construct_data(Archive&,
                           "",
                           MessagePort(0),
                           XmlRpcPort(0),
-                          FailoverCachePort(0));
+                          FailoverCachePort(0),
+                          youtils::Uri());
 
     new(status) ClusterNodeStatus(cfg,
                                   ClusterNodeStatus::State::Offline);

src/filesystem/test/ClusterRegistryTest.cpp
@@ -61,7 +61,8 @@ protected:
                                                      n,
                                                      vfs::MessagePort(i + 1),
                                                      vfs::XmlRpcPort(i + 1),
-                                                     vfs::FailoverCachePort(i + 1)));
+                                                     vfs::FailoverCachePort(i + 1),
+                                                     boost::none));
         }
 
         return configs;

src/filesystem/test/FileSystemTestSetup.cpp
@@ -385,12 +385,20 @@ bpt::ptree&
 FileSystemTestSetup::make_edge_config_(bpt::ptree& pt,
 				       const vfs::NodeId& node_id)
 {
-    const std::string uri(edge_transport() + "://"s + address() + ":"s +
-			  boost::lexical_cast<std::string>(node_id == local_node_id() ?
-							   local_edge_port() :
-							   remote_edge_port()));
-    ip::PARAMETER_TYPE(network_uri)(uri).persist(pt);
+    const yt::Uri uri(make_edge_uri_(node_id));
+    ip::PARAMETER_TYPE(network_uri)(boost::lexical_cast<std::string>(uri)).persist(pt);
     return pt;
+}
+
+yt::Uri
+FileSystemTestSetup::make_edge_uri_(const vfs::NodeId& node_id)
+{
+    return yt::Uri()
+        .scheme(edge_transport())
+        .host(address())
+        .port(node_id == local_node_id() ?
+              local_edge_port() :
+              remote_edge_port());
 }
 
 void

src/filesystem/test/FileSystemTestSetup.h
@@ -173,6 +173,9 @@ protected:
     make_edge_config_(boost::property_tree::ptree& pt,
                       const volumedriverfs::NodeId& vrouter_id);
 
+    static youtils::Uri
+    make_edge_uri_(const volumedriverfs::NodeId&);
+
     void
     make_dirs_(const boost::filesystem::path& topdir) const;
 
@@ -274,7 +277,8 @@ protected:
                                                  address(),
                                                  volumedriverfs::MessagePort(port_base() + 1),
                                                  volumedriverfs::XmlRpcPort(port_base() + 3),
-                                                 volumedriverfs::FailoverCachePort(port_base() + 5));
+                                                 volumedriverfs::FailoverCachePort(port_base() + 5),
+                                                 make_edge_uri_(local_node_id()));
     }
 
     static volumedriverfs::ClusterNodeConfig
@@ -284,7 +288,8 @@ protected:
                                                  address(),
                                                  volumedriverfs::MessagePort(port_base() + 2),
                                                  volumedriverfs::XmlRpcPort(port_base() + 4),
-                                                 volumedriverfs::FailoverCachePort(port_base() + 6));
+                                                 volumedriverfs::FailoverCachePort(port_base() + 6),
+                                                 make_edge_uri_(remote_node_id()));
     }
 
     static uint16_t

src/filesystem/test/ObjectRouterTest.cpp
@@ -181,7 +181,8 @@ public:
                        local_config().host,
                        vfs::MessagePort(local_config().message_port + next_port_off_),
                        vfs::XmlRpcPort(local_config().xmlrpc_port + next_port_off_),
-                       vfs::FailoverCachePort(local_config().failovercache_port + next_port_off_));
+                       vfs::FailoverCachePort(local_config().failovercache_port + next_port_off_),
+                       make_edge_uri_(local_node_id()));
 
         ++next_port_off_;
 
@@ -531,7 +532,8 @@ TEST_F(ObjectRouterTest, no_modification_of_existing_node_config)
                                    remote_config().host,
                                    vfs::MessagePort(remote_config().message_port + 1),
                                    remote_config().xmlrpc_port,
-                                   remote_config().failovercache_port) };
+                                   remote_config().failovercache_port,
+                                   remote_config().network_server_uri) };
 
     registry->erase_node_configs();
     registry->set_node_configs(configs);

commit b03bd4d11a8a104e0d74dbfd1da07dbb662ee6d7

filesystem/ClusterNodeConfig: fix backward compatibility
Regression introduced with
   5f283de3fd0d41943d22219094e6935e783b520f
   filesystem/ClusterNodeConfig: introduce network_server_uri member
.

Addresses https://github.com/openvstorage/volumedriver/issues/112

src/filesystem/ClusterNodeConfig.h
@@ -114,9 +114,9 @@ struct ClusterNodeConfig
 
         ar & const_cast<NodeId&>(vrouter_id);
         ar & const_cast<std::string&>(host);
-        ar & const_cast<MessagePort&>(message_port);
-        ar & const_cast<XmlRpcPort&>(xmlrpc_port);
-        ar & const_cast<FailoverCachePort&>(failovercache_port);
+        ar & static_cast<uint16_t&>(const_cast<MessagePort&>(message_port));
+        ar & static_cast<uint16_t&>(const_cast<XmlRpcPort&>(xmlrpc_port));
+        ar & static_cast<uint16_t&>(const_cast<FailoverCachePort&>(failovercache_port));
 
         if (version > 1)
         {
@@ -136,9 +136,9 @@ struct ClusterNodeConfig
 
         ar & vrouter_id;
         ar & host;
-        ar & message_port;
-        ar & xmlrpc_port;
-        ar & failovercache_port;
+        ar & static_cast<const uint16_t&>(message_port);
+        ar & static_cast<const uint16_t&>(xmlrpc_port);
+        ar & static_cast<const uint16_t&>(failovercache_port);
         ar & network_server_uri;
     }
 

src/youtils/test/OurStrongTypedefTest.cpp
@@ -204,4 +204,17 @@ TEST_F(OurStrongTypedefTest, binary_serialization)
                        ba::binary_iarchive>(UnsignedInt(0xbeefcafe));
 }
 
+TEST_F(OurStrongTypedefTest, text_deserialization_from_underlying_type)
+{
+    std::stringstream ss;
+
+    const uint16_t u16 = 42;
+
+    ba::text_oarchive(ss) << u16;
+
+    UnsignedShort us;
+    EXPECT_THROW(ba::text_iarchive(ss) >> us,
+                 std::exception);
+}
+
 }