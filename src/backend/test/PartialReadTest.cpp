5540acd15b88ecf2f429931d3dfea48acc7634d4
Arne Redlich <arne.redlich@openvstorage.com>
2016-09-12 13:05:24 +0200

    backend/test: introduce PartialReadTest.performance
    Disabled by default.

--- src/backend/test/PartialReadTest.cpp
+++ src/backend/test/PartialReadTest.cpp
@@ -13,16 +13,21 @@
 // Open vStorage is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY of any kind.
 
-#include <gtest/gtest.h>
 
 #include "BackendTestBase.h"
 
 #include "../BackendConnectionInterface.h"
 #include "../SimpleFetcher.h"
 
+#include <boost/thread.hpp>
+
+#include <gtest/gtest.h>
+
 #include <youtils/SourceOfUncertainty.h>
 #include <youtils/FileUtils.h>
 #include <youtils/FileDescriptor.h>
+#include <youtils/System.h>
+#include <youtils/Timer.h>
 
 namespace backendtest
 {
@@ -45,7 +50,7 @@ public:
     void
     SetUp() override final
     {
-BackendTestBase::SetUp();
+        BackendTestBase::SetUp();
         fs::create_directories(partial_read_cache_);
 
         nspace_ = make_random_namespace();
@@ -82,6 +87,8 @@ BackendTestBase::SetUp();
     }
 
 protected:
+    DECLARE_LOGGER("PartialReadTest");
+
     const fs::path partial_read_cache_;
     std::unique_ptr<WithRandomNamespace> nspace_;
 };
@@ -254,6 +261,178 @@ TEST_P(PartialReadTest, multiple_objects)
     partial_read_and_check(partial_reads,
                            buf,
                            the_buffer);
+}
+
+TEST_P(PartialReadTest, DISABLED_performance)
+{
+    const size_t nnspaces = yt::System::get_env_with_default("BACKEND_PARTIAL_READ_TEST_NAMESPACES",
+                                                             1);
+
+    ASSERT_NE(0,
+              nnspaces) << "fix your test";
+
+    const size_t nthreads = yt::System::get_env_with_default("BACKEND_PARTIAL_READ_TEST_THREADS",
+                                                             1);
+
+    ASSERT_NE(0,
+              nthreads);
+    EXPECT_LE(nnspaces,
+              nthreads) << "having more namespaces than threads does not make sense for this test";
+
+    const size_t nobjs = yt::System::get_env_with_default("BACKEND_PARTIAL_READ_TEST_OBJECT_COUNT",
+                                                          1);
+    ASSERT_NE(0,
+              nobjs);
+
+    const size_t obj_size = yt::System::get_env_with_default("BACKEND_PARTIAL_READ_TEST_OBJECT_SIZE",
+                                                            4ULL << 20);
+    ASSERT_NE(0,
+              obj_size);
+
+    const size_t slice_size = yt::System::get_env_with_default("BACKEND_PARTIAL_READ_TEST_SLICE_SIZE",
+                                                               4ULL << 10);
+    ASSERT_NE(0,
+              slice_size);
+    ASSERT_EQ(0,
+              obj_size % slice_size);
+
+    const size_t nslices = yt::System::get_env_with_default("BACKEND_PARTIAL_READ_TEST_SLICE_COUNT",
+                                                            1);
+
+    const size_t iterations = yt::System::get_env_with_default("BACKEND_PARTIAL_READ_TEST_ITERATIONS",
+                                                               1ULL << 10);
+    ASSERT_NE(0,
+              iterations);
+
+    std::vector<std::unique_ptr<WithRandomNamespace>> nspaces;
+    nspaces.reserve(nnspaces);
+
+    for (size_t i = 0; i < nnspaces; ++i)
+    {
+        nspaces.emplace_back(make_random_namespace());
+    }
+
+    {
+        const fs::path path(yt::FileUtils::create_temp_file_in_temp_dir("some-object"));
+        ALWAYS_CLEANUP_FILE(path);
+
+        {
+            const std::vector<uint8_t> buf(obj_size);
+
+            FileDescriptor io(path,
+                              FDMode::Write);
+            io.write(buf.data(),
+                     buf.size());
+        }
+
+        std::vector<boost::thread> threads;
+        threads.reserve(nspaces.size());
+
+        auto upload([&](const Namespace& nspace)
+                    {
+                        BackendInterfacePtr bi(bi_(nspace));
+
+                        for (size_t i = 0; i < nobjs; ++i)
+                        {
+                            bi->write(path,
+                                      boost::lexical_cast<std::string>(i),
+                                      OverwriteObject::F);
+                        }
+                    });
+
+        yt::SteadyTimer t;
+        for (size_t i = 0; i < nspaces.size(); ++i)
+        {
+            threads.emplace_back(boost::thread([&, i]
+                                               {
+                                                   upload(nspaces[i]->ns());
+                                               }));
+        }
+
+        for (auto& t : threads)
+        {
+            t.join();
+        }
+
+        const auto elapsed = t.elapsed();
+
+        std::cout << "Upload of " << nobjs << " objects per namespace of size " << obj_size << " to " <<
+            nspaces.size() << " namespaces took " << elapsed << std::endl;
+    }
+
+    auto partial_read([&](SourceOfUncertainty& sou,
+                          std::vector<uint8_t>& buf,
+                          SimpleFetcher& fetch,
+                          BackendInterfacePtr& bi)
+                      {
+                          ObjectSlices slices;
+
+                          off_t off = sou(0UL,
+                                          obj_size / slice_size - 1) * slice_size;
+
+                          for (size_t i = 0; i < nslices; ++i)
+                          {
+                              ASSERT_TRUE(slices.emplace(slice_size,
+                                                         (off + i * slice_size) % obj_size,
+                                                         buf.data() + i * slice_size).second);
+                          }
+
+                          const std::string oname(boost::lexical_cast<std::string>(sou(0UL,
+                                                                                       nobjs - 1)));
+                          const PartialReads partial_reads{{oname, std::move(slices)}};
+
+                          bi->partial_read(partial_reads,
+                                           fetch,
+                                           GetParam());
+                      });
+
+    auto work([&](const size_t id)
+              {
+                  const Namespace& nspace(nspaces[id % nspaces.size()]->ns());
+                  BackendInterfacePtr bi(bi_(nspace));
+                  SourceOfUncertainty sou;
+                  BackendConnectionInterfacePtr conn(cm_->getConnection());
+                  SimpleFetcher fetch(*conn,
+                                      nspace_->ns(),
+                                      partial_read_cache_);
+
+                  std::vector<uint8_t> buf(nslices * slice_size);
+
+                  for (size_t i = 0; i < iterations; ++i)
+                  {
+                      partial_read(sou,
+                                   buf,
+                                   fetch,
+                                   bi);
+                  }
+              });
+
+    std::vector<boost::thread> threads;
+    threads.reserve(nthreads);
+
+    yt::SteadyTimer t;
+    for (size_t i = 0; i < nthreads; ++i)
+    {
+        threads.emplace_back(boost::thread([&, i]
+                                           {
+                                               work(i);
+                                           }));
+    }
+
+    for (auto& t : threads)
+    {
+        t.join();
+    }
+
+    const auto elapsed = t.elapsed();
+    double secs(boost::chrono::duration_cast<boost::chrono::nanoseconds>(elapsed).count() / 1e9);
+    ASSERT_NE(0,
+              secs);
+
+    std::cout << nthreads << " threads doing " << iterations << " partial reads of " <<
+        nslices << " slices of size " << slice_size << " took " <<  elapsed <<
+        " (" << secs << " secs) -> " << (nthreads * iterations / secs) << " IOPS" <<
+        std::endl;
 }
 
 INSTANTIATE_TEST_CASE_P(PartialReadTests,
