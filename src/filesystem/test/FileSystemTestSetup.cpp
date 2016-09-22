dfbd1951985465918d19782563c39ebd3256d5d7
Arne Redlich <arne.redlich@openvstorage.com>
2016-09-22 09:38:29 +0200

    FileSystemTestSetup: support for configuring the DTL config mode
    Part of https://github.com/openvstorage/volumedriver/issues/113

--- src/filesystem/test/FileSystemTestSetup.cpp
+++ src/filesystem/test/FileSystemTestSetup.cpp
@@ -80,6 +80,8 @@ FileSystemTestSetup::FileSystemTestSetup(const FileSystemTestSetupParameters& pa
     , redirect_timeout_ms_(params.redirect_timeout_ms_)
     , redirect_retries_(params.redirect_retries_)
     , scrub_manager_interval_secs_(params.scrub_manager_interval_secs_)
+    , dtl_config_mode_(params.dtl_config_mode_)
+    , dtl_mode_(params.dtl_mode_)
     , fdriver_namespace_("ovs-fdnspc-fstest-"s + yt::UUID().str())
     , arakoon_test_setup_(std::make_shared<ara::ArakoonTestSetup>(topdir_ / "arakoon"))
     , client_(vrouter_cluster_id(),
@@ -258,6 +260,25 @@ FileSystemTestSetup::make_mdstore_config_(bpt::ptree& pt) const
 }
 
 bpt::ptree&
+FileSystemTestSetup::make_dtl_config_(const vfs::NodeId& vrouter_id,
+                                      bpt::ptree& pt) const
+{
+    ip::PARAMETER_TYPE(fs_dtl_config_mode)(dtl_config_mode_).persist(pt);
+    if (dtl_config_mode_ == vfs::FailOverCacheConfigMode::Manual)
+    {
+        const vfs::ClusterNodeConfig cfg(vrouter_id == local_node_id() ?
+                                         local_config() :
+                                         remote_config());
+
+        ip::PARAMETER_TYPE(fs_dtl_host)(cfg.host).persist(pt);
+        ip::PARAMETER_TYPE(fs_dtl_port)(cfg.failovercache_port).persist(pt);
+        ip::PARAMETER_TYPE(fs_dtl_mode)(dtl_mode_).persist(pt);
+    }
+
+    return pt;
+}
+
+bpt::ptree&
 FileSystemTestSetup::make_config_(bpt::ptree& pt,
                                   const fs::path& topdir,
                                   const vfs::NodeId& vrouter_id) const
@@ -341,6 +362,8 @@ FileSystemTestSetup::make_config_(bpt::ptree& pt,
         ip::PARAMETER_TYPE(fs_enable_network_interface)(true).persist(pt);
 
         make_mdstore_config_(pt);
+        make_dtl_config_(vrouter_id,
+                         pt);
     }
 
     // volume_router
