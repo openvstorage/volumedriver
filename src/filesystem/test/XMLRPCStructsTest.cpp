openvstorage/volumedriver

commit 4d10e9674e7290ff780ca4db3c3bb50c4adf8f58

Restore backward compatibility of PerformanceCounters
Since the XMLRPC client/server communication uses binary boost
serialization archives, new fields can only be added in certain
cases (at the end of an archive). With the recent modifications (new
fields added to PerformanceCounter types of which multiple instances are
collected in struct PerformanceCounters, distribution info for
PerformanceCounter types), backward compatibility cannot be maintained
by using binary archives.
To address this, the old calls are replaced (for the time being they
will be still available but will eventually be phased out) with new
versions that use boost serialization's XML archives.

Other calls also use the binary serialization so once changes become
necessary these will have to follow this example, or the XMLRPC + boost
serialization based communication is replaced with something less
verbose.

Part of https://github.com/openvstorage/volumedriver/issues/91

src/filesystem/XMLRPCStructs.h
@@ -25,6 +25,7 @@
 #include <boost/archive/binary_oarchive.hpp>
 #include <boost/serialization/shared_ptr.hpp>
 #include <boost/serialization/unique_ptr.hpp>
+#include <boost/variant.hpp>
 
 #include <youtils/Serialization.h>
 
@@ -39,40 +40,127 @@
 namespace volumedriverfs
 {
 
-struct
-XMLRPCStructs
+template<typename T>
+struct SerializationName
 {
-    typedef boost::archive::binary_iarchive iarchive_type;
-    typedef boost::archive::binary_oarchive oarchive_type;
+    static const char*
+    name()
+    {
+        return T::serialization_name;
+    }
+};
 
-    template<typename T>
+template<>
+struct SerializationName<boost::variant<youtils::UpdateReport,
+                                        youtils::ConfigurationReport>>
+{
+    static const char*
+    name()
+    {
+        return "ConfigurationUpdateResult";
+    }
+};
+
+template<typename T>
+struct SerializationName<std::unique_ptr<T>>
+{
+    static const char*
+    name()
+    {
+        return SerializationName<T>::name();
+    }
+};
+
+template<>
+struct SerializationName<volumedriver::MetaDataBackendConfig>
+{
+    static const char*
+    name()
+    {
+        return "MetaDataBackendConfig";
+    }
+};
+
+class ClusterNodeStatus;
+
+template<>
+struct SerializationName<std::map<NodeId, ClusterNodeStatus>>
+{
+    static const char*
+    name()
+    {
+        return "ClusterNodeStatusMap";
+    }
+};
+
+template<typename iarchive_type, typename oarchive_type>
+struct XMLRPCStructsT
+{
+    template<typename T, typename SerName = SerializationName<T>>
     static ::XmlRpc::XmlRpcValue
-    serialize_to_xmlrpc_value(const T& the_value)
+    serialize_to_xmlrpc_value(const T& t)
     {
         std::stringstream ss;
         oarchive_type oa(ss);
-        oa << the_value;
-
+        oa & boost::serialization::make_nvp(SerName::name(),
+                                            t);
         return ::XmlRpc::XmlRpcValue(static_cast<const void*>(ss.str().c_str()),
                                      ss.str().size());
     }
 
-    template<typename T>
+    template<typename T, typename SerName = SerializationName<T>>
     static T
     deserialize_from_xmlrpc_value(XmlRpc::XmlRpcValue& xval)
     {
-        T return_value;
+        T t;
         std::stringstream ss;
         XmlRpc::XmlRpcValue::BinaryData data = xval;
         ss.str(std::string(data.begin(), data.end()));
 
         iarchive_type ia(ss);
-        ia >> return_value;
-        return return_value;
+        ia & boost::serialization::make_nvp(SerName::name(),
+                                            t);
+        return t;
+    }
+};
+
+// The old flavour - it uses binary archive which are not future-proof. Phase out their
+// use as-needed.
+using XMLRPCStructsBinary = XMLRPCStructsT<boost::archive::binary_iarchive,
+                                           boost::archive::binary_oarchive>;
+
+// Use this for new code.
+using XMLRPCStructsXML = XMLRPCStructsT<boost::archive::xml_iarchive,
+                                        boost::archive::xml_oarchive>;
+
+//Just borrowing boost's semi human readable xml serialization to support printing these objects in python.
+//This is only intended for interactive python sessions (debugging/operations).
+//Production client code should of course directly use the properties of the object.
+template<typename T>
+struct XMLRPCSerializer
+{
+    void
+    set_from_str(const std::string& str)
+    {
+        std::stringstream ss;
+        typename T::iarchive_type ia(ss);
+        ia & boost::serialization::make_nvp(T::serialization_name,
+                                            static_cast<T&>(*this));
+    }
+
+    std::string
+    str() const
+    {
+        std::stringstream ss;
+        youtils::Serialization::serializeNVPAndFlush<typename T::oarchive_type>(ss,
+                                                                                T::serialization_name,
+                                                                                static_cast<const T&>(*this));
+        return ss.str();
     }
 };
 
 struct XMLRPCVolumeInfo
+    : public XMLRPCSerializer<XMLRPCVolumeInfo>
 {
     typedef boost::archive::xml_oarchive oarchive_type;
     typedef boost::archive::xml_iarchive iarchive_type;
@@ -140,7 +228,6 @@ struct XMLRPCVolumeInfo
 #undef EQ
     }
 
-
     friend class boost::serialization::access;
 
     template<typename Archive>
@@ -178,20 +265,95 @@ struct XMLRPCVolumeInfo
     }
 
     static constexpr const char* serialization_name =  "XMLRPCVolumeInfo";
-
-    void
-    set_from_str(const std::string& str);
-
-    std::string
-    str() const;
 };
 
-// No reference to "volume" in XMLRPCStatistics structname or python classname as we
-// could reuse this struct for reporting aggregate values (per VSA / vPool)
-struct XMLRPCStatistics
+// No reference to "volume" in XMLRPCStatisticsV2 structname or python classname as we
+// could reuse this struct for reporting aggregate values (per VSA / vPool).
+// This one is deprecated - there's a new version of this (XMLRPCStatistics).
+struct XMLRPCStatisticsV2
+    : public XMLRPCSerializer<XMLRPCStatisticsV2>
 {
     typedef boost::archive::xml_oarchive oarchive_type;
     typedef boost::archive::xml_iarchive iarchive_type;
+
+    uint64_t sco_cache_hits = 0;
+    uint64_t sco_cache_misses = 0;
+    uint64_t cluster_cache_hits = 0;
+    uint64_t cluster_cache_misses = 0;
+    uint64_t metadata_store_hits = 0;
+    uint64_t metadata_store_misses = 0;
+    uint64_t stored = 0;
+
+    volumedriver::PerformanceCountersV1 performance_counters;
+
+    bool
+    operator==(const XMLRPCStatisticsV2& other) const
+    {
+#define EQ(x)                                   \
+        (x == other.x)
+        return
+            EQ(sco_cache_hits) and
+            EQ(sco_cache_misses) and
+            EQ(cluster_cache_hits) and
+            EQ(cluster_cache_misses) and
+            EQ(metadata_store_hits) and
+            EQ(metadata_store_misses) and
+            EQ(stored) and
+            EQ(performance_counters)
+            ;
+#undef EQ
+    }
+
+    template<typename Archive>
+    void
+    serialize(Archive& ar, const unsigned int version)
+    {
+        ar & BOOST_SERIALIZATION_NVP(sco_cache_hits);
+        ar & BOOST_SERIALIZATION_NVP(sco_cache_misses);
+        ar & BOOST_SERIALIZATION_NVP(cluster_cache_hits);
+        ar & BOOST_SERIALIZATION_NVP(cluster_cache_misses);
+        ar & BOOST_SERIALIZATION_NVP(metadata_store_hits);
+        ar & BOOST_SERIALIZATION_NVP(metadata_store_misses);
+        ar & BOOST_SERIALIZATION_NVP(performance_counters);
+
+        if (version > 1)
+        {
+            ar & BOOST_SERIALIZATION_NVP(stored);
+        }
+    }
+
+    static constexpr const char* serialization_name =  "XMLRPCStatistics";
+};
+
+// largely duplicates XMLRPCStatisticsV2 above which is kept
+// around for backward compat purposes.
+struct XMLRPCStatistics
+    : public XMLRPCSerializer<XMLRPCStatistics>
+{
+    XMLRPCStatistics() = default;
+
+    ~XMLRPCStatistics() = default;
+
+    XMLRPCStatistics(const XMLRPCStatistics&) = default;
+
+    XMLRPCStatistics&
+    operator=(const XMLRPCStatistics&) = default;
+
+    XMLRPCStatistics(const XMLRPCStatisticsV2& old)
+        : sco_cache_hits(old.sco_cache_hits)
+        , sco_cache_misses(old.sco_cache_misses)
+        , cluster_cache_hits(old.cluster_cache_hits)
+        , cluster_cache_misses(old.cluster_cache_misses)
+        , metadata_store_hits(old.metadata_store_hits)
+        , metadata_store_misses(old.metadata_store_misses)
+        , stored(old.stored)
+        , partial_read_fast(0)
+        , partial_read_slow(0)
+        , performance_counters(static_cast<volumedriver::PerformanceCounters>(old.performance_counters))
+    {}
+
+    using iarchive_type = boost::archive::xml_iarchive;
+    using oarchive_type = boost::archive::xml_oarchive;
 
     uint64_t sco_cache_hits = 0;
     uint64_t sco_cache_misses = 0;
@@ -218,9 +380,9 @@ struct XMLRPCStatistics
             EQ(metadata_store_hits) and
             EQ(metadata_store_misses) and
             EQ(stored) and
+            EQ(performance_counters) and
             EQ(partial_read_fast) and
-            EQ(partial_read_slow) and
-            EQ(performance_counters)
+            EQ(partial_read_slow)
             ;
 #undef EQ
     }
@@ -249,16 +411,11 @@ struct XMLRPCStatistics
         }
     }
 
-    static constexpr const char* serialization_name =  "XMLRPCStatistics";
-
-    void
-    set_from_str(const std::string& str);
-
-    std::string
-    str() const;
+    static constexpr const char* serialization_name = "XMLRPCStatisticsV2";
 };
 
 struct XMLRPCSnapshotInfo
+    : public XMLRPCSerializer<XMLRPCSnapshotInfo>
 {
     typedef boost::archive::xml_oarchive oarchive_type;
     typedef boost::archive::xml_iarchive iarchive_type;
@@ -331,15 +488,10 @@ struct XMLRPCSnapshotInfo
     }
 
     static constexpr const char* serialization_name = "XMLRPCSnapshotInfo";
-
-    void
-    set_from_str(const std::string& str);
-
-    std::string
-    str() const;
 };
 
 struct XMLRPCClusterCacheHandleInfo
+    : public XMLRPCSerializer<XMLRPCClusterCacheHandleInfo>
 {
     typedef boost::archive::xml_oarchive oarchive_type;
     typedef boost::archive::xml_iarchive iarchive_type;
@@ -397,17 +549,12 @@ struct XMLRPCClusterCacheHandleInfo
     }
 
     static constexpr const char* serialization_name = "XMLRPCClusterCacheHandleInfo";
-
-    void
-    set_from_str(const std::string& str);
-
-    std::string
-    str() const;
 };
 
 }
 
 BOOST_CLASS_VERSION(volumedriverfs::XMLRPCVolumeInfo, 2);
+BOOST_CLASS_VERSION(volumedriverfs::XMLRPCStatisticsV2, 2);
 BOOST_CLASS_VERSION(volumedriverfs::XMLRPCStatistics, 3);
 BOOST_CLASS_VERSION(volumedriverfs::XMLRPCSnapshotInfo, 2);
 BOOST_CLASS_VERSION(volumedriverfs::XMLRPCClusterCacheHandleInfo, 1);

src/filesystem/Makefile.am
@@ -110,7 +110,6 @@ libvolumedriverfs_la_SOURCES = \
 		VirtualDiskFormatRaw.cpp \
 		XMLRPC.cpp \
 		XMLRPCKeys.cpp \
-		XMLRPCStructs.cpp \
 		XMLRPCUtils.cpp \
 		ZWorkerPool.cpp
 

src/filesystem-python-client/Piccalilli.h
@@ -92,7 +92,7 @@ struct PickleTraits<volumedriver::PerformanceCounter<T, BucketTraits>>
     {
         std::stringstream ss;
         OArchive oa(ss);
-        oa << boost::serialization::make_nvp("PerformanceCounterU64",
+        oa << boost::serialization::make_nvp("PerformanceCounter",
                                              c);
         return ss.str();
     }
@@ -103,7 +103,7 @@ struct PickleTraits<volumedriver::PerformanceCounter<T, BucketTraits>>
     {
         std::stringstream ss(s);
         IArchive ia(ss);
-        ia >> boost::serialization::make_nvp("PerformanceCounterU64",
+        ia >> boost::serialization::make_nvp("PerformanceCounter",
                                              c);
     }
 };

src/filesystem/LocalPythonClient.cpp
@@ -105,7 +105,7 @@ LocalPythonClient::update_configuration(const std::string& path,
     XmlRpc::XmlRpcValue req;
     req[XMLRPCKeys::configuration_path] = path;
     auto rsp(call(UpdateConfiguration::method_name(), req, timeout));
-    const auto res(XMLRPCStructs::deserialize_from_xmlrpc_value<boost::variant<yt::UpdateReport,
+    const auto res(XMLRPCStructsBinary::deserialize_from_xmlrpc_value<boost::variant<yt::UpdateReport,
                    yt::ConfigurationReport>>(rsp));
 
     ReportVisitor v;
@@ -219,7 +219,7 @@ LocalPythonClient::get_cluster_cache_handle_info(const vd::ClusterCacheHandle ha
 
     auto rsp(call(GetClusterCacheHandleInfo::method_name(), req, timeout));
     return
-        XMLRPCStructs::deserialize_from_xmlrpc_value<XMLRPCClusterCacheHandleInfo>(rsp);
+        XMLRPCStructsBinary::deserialize_from_xmlrpc_value<XMLRPCClusterCacheHandleInfo>(rsp);
 }
 
 void

src/filesystem/PythonClient.cpp
@@ -524,7 +524,7 @@ PythonClient::list_client_connections(const std::string& node_id,
     std::vector<ClientInfo> info;
     for (auto i = 0; i < rsp.size(); ++i)
     {
-        info.push_back(XMLRPCStructs::deserialize_from_xmlrpc_value<ClientInfo>(rsp[i]));
+        info.push_back(XMLRPCStructsBinary::deserialize_from_xmlrpc_value<ClientInfo>(rsp[i]));
     }
     return info;
 }
@@ -540,7 +540,7 @@ PythonClient::info_snapshot(const std::string& volume_id,
     req[XMLRPCKeys::snapshot_id] = snapshot_id;
 
     auto rsp(call(SnapshotInfo::method_name(), req, timeout));
-    return XMLRPCStructs::deserialize_from_xmlrpc_value<XMLRPCSnapshotInfo>(rsp);
+    return XMLRPCStructsBinary::deserialize_from_xmlrpc_value<XMLRPCSnapshotInfo>(rsp);
 }
 
 XMLRPCVolumeInfo
@@ -551,7 +551,7 @@ PythonClient::info_volume(const std::string& volume_id,
 
     req[XMLRPCKeys::volume_id] = volume_id;
     auto rsp(call(VolumeInfo::method_name(), req, timeout));
-    return XMLRPCStructs::deserialize_from_xmlrpc_value<XMLRPCVolumeInfo>(rsp);
+    return XMLRPCStructsBinary::deserialize_from_xmlrpc_value<XMLRPCVolumeInfo>(rsp);
 }
 
 XMLRPCStatistics
@@ -566,8 +566,18 @@ PythonClient::statistics_volume(const std::string& volume_id,
                      XMLRPCKeys::reset,
                      reset);
 
-    auto rsp(call(VolumePerformanceCounters::method_name(), req, timeout));
-    return XMLRPCStructs::deserialize_from_xmlrpc_value<XMLRPCStatistics>(rsp);
+    return fallback_<XMLRPCStatistics>(VolumePerformanceCountersV3::method_name(),
+                                       VolumePerformanceCounters::method_name(),
+                                       req,
+                                       timeout,
+                                       [](XmlRpc::XmlRpcValue& rsp) -> XMLRPCStatistics
+        {
+            return XMLRPCStructsXML::deserialize_from_xmlrpc_value<XMLRPCStatistics>(rsp);
+        },
+                                         [](XmlRpc::XmlRpcValue& rsp) -> XMLRPCStatistics
+        {
+            return XMLRPCStructsBinary::deserialize_from_xmlrpc_value<XMLRPCStatisticsV2>(rsp);
+        });
 }
 
 XMLRPCStatistics
@@ -582,8 +592,18 @@ PythonClient::statistics_node(const std::string& node_id,
                      XMLRPCKeys::reset,
                      reset);
 
-    auto rsp(call(VolumeDriverPerformanceCounters::method_name(), req, timeout));
-    return XMLRPCStructs::deserialize_from_xmlrpc_value<XMLRPCStatistics>(rsp);
+    return fallback_<XMLRPCStatistics>(VolumeDriverPerformanceCountersV3::method_name(),
+                                       VolumeDriverPerformanceCounters::method_name(),
+                                       req,
+                                       timeout,
+                                       [](XmlRpc::XmlRpcValue& rsp) -> XMLRPCStatistics
+        {
+            return XMLRPCStructsXML::deserialize_from_xmlrpc_value<XMLRPCStatistics>(rsp);
+        },
+                                         [](XmlRpc::XmlRpcValue& rsp) -> XMLRPCStatistics
+        {
+            return XMLRPCStructsBinary::deserialize_from_xmlrpc_value<XMLRPCStatisticsV2>(rsp);
+        });
 }
 
 boost::optional<vd::VolumeId>
@@ -655,7 +675,7 @@ PythonClient::create_volume(const std::string& target_path,
     if (mdb_config)
     {
         req[XMLRPCKeys::metadata_backend_config] =
-            XMLRPCStructs::serialize_to_xmlrpc_value(mdb_config->clone());
+            XMLRPCStructsBinary::serialize_to_xmlrpc_value(mdb_config->clone());
     }
 
     if (not node_id.empty())
@@ -696,7 +716,7 @@ PythonClient::create_clone(const std::string& target_path,
     if (mdb_config)
     {
         req[XMLRPCKeys::metadata_backend_config] =
-            XMLRPCStructs::serialize_to_xmlrpc_value(mdb_config->clone());
+            XMLRPCStructsBinary::serialize_to_xmlrpc_value(mdb_config->clone());
     }
 
     if (not node_id.empty())
@@ -743,7 +763,7 @@ PythonClient::update_metadata_backend_config(const std::string& volume_id,
 
     req[XMLRPCKeys::volume_id] = volume_id;
     req[XMLRPCKeys::metadata_backend_config] =
-        XMLRPCStructs::serialize_to_xmlrpc_value(mdb_config->clone());
+        XMLRPCStructsBinary::serialize_to_xmlrpc_value(mdb_config->clone());
 
     call(UpdateMetaDataBackendConfig::method_name(), req, timeout);
 }
@@ -964,7 +984,7 @@ PythonClient::info_cluster(const MaybeSeconds& timeout)
 {
     XmlRpc::XmlRpcValue req;
     auto rsp(call(GetNodesStatusMap::method_name(), req, timeout));
-    ClusterRegistry::NodeStatusMap nodestatusmap(XMLRPCStructs::deserialize_from_xmlrpc_value<ClusterRegistry::NodeStatusMap>(rsp));
+    ClusterRegistry::NodeStatusMap nodestatusmap(XMLRPCStructsBinary::deserialize_from_xmlrpc_value<ClusterRegistry::NodeStatusMap>(rsp));
 
     std::map<NodeId, ClusterNodeStatus::State> ret;
     for (const auto& stat : nodestatusmap)
@@ -1146,6 +1166,13 @@ PythonClient::get_backend_connection_pool(const ObjectId& volume_id,
     req[XMLRPCKeys::volume_id] = volume_id.str();
     auto rsp(call(GetBackendConnectionPool::method_name(), req, timeout));
     return static_cast<std::string>(rsp[XMLRPCKeys::backend_config_str]);
+}
+
+std::vector<std::string>
+PythonClient::list_methods(const MaybeSeconds& timeout)
+{
+    XmlRpc::XmlRpcValue req;
+    return extract_vec(call("system.listMethods", req, timeout));
 }
 
 }

src/filesystem/XMLRPC.cpp
@@ -919,7 +919,7 @@ VolumeInfo::execute_internal(::XmlRpc::XmlRpcValue& params,
         std::string("");
     volume_info.vrouter_id = reg->node_id;
 
-    result = XMLRPCStructs::serialize_to_xmlrpc_value(volume_info);
+    result = XMLRPCStructsBinary::serialize_to_xmlrpc_value(volume_info);
 }
 
 void
@@ -953,7 +953,7 @@ UpdateMetaDataBackendConfig::execute_internal(::XmlRpc::XmlRpcValue& params,
     {
         const vd::VolumeId volume_id(getID(params[0]));
         vd::VolumeConfig::MetaDataBackendConfigPtr mdb_config;
-        mdb_config = XMLRPCStructs::deserialize_from_xmlrpc_value<decltype(mdb_config)>(params[0][XMLRPCKeys::metadata_backend_config]);
+        mdb_config = XMLRPCStructsBinary::deserialize_from_xmlrpc_value<decltype(mdb_config)>(params[0][XMLRPCKeys::metadata_backend_config]);
 
         api::updateMetaDataBackendConfig(volume_id,
                                          *mdb_config);
@@ -1000,7 +1000,7 @@ SnapshotInfo::execute_internal(::XmlRpc::XmlRpcValue& params,
                                            snap.inBackend(),
                                            std::string(meta.begin(), meta.end()));
 
-        result = XMLRPCStructs::serialize_to_xmlrpc_value(snap_info);
+        result = XMLRPCStructsBinary::serialize_to_xmlrpc_value(snap_info);
     });
 }
 
@@ -1018,7 +1018,7 @@ VolumeCreate::execute_internal(::XmlRpc::XmlRpcValue& params,
     if (params[0].hasMember(XMLRPCKeys::metadata_backend_config))
     {
         mdb_config =
-            XMLRPCStructs::deserialize_from_xmlrpc_value<decltype(mdb_config)>(params[0][XMLRPCKeys::metadata_backend_config]);
+            XMLRPCStructsBinary::deserialize_from_xmlrpc_value<decltype(mdb_config)>(params[0][XMLRPCKeys::metadata_backend_config]);
     }
 
     const ObjectId id(fs_.create_volume(path,
@@ -1054,7 +1054,7 @@ VolumeClone::execute_internal(::XmlRpc::XmlRpcValue& params,
     if (params[0].hasMember(XMLRPCKeys::metadata_backend_config))
     {
         mdb_config =
-            XMLRPCStructs::deserialize_from_xmlrpc_value<decltype(mdb_config)>(params[0][XMLRPCKeys::metadata_backend_config]);
+            XMLRPCStructsBinary::deserialize_from_xmlrpc_value<decltype(mdb_config)>(params[0][XMLRPCKeys::metadata_backend_config]);
     }
 
     const ObjectId id(fs_.create_clone(path,
@@ -1090,12 +1090,49 @@ void
 VolumePerformanceCounters::execute_internal(XmlRpc::XmlRpcValue& params,
                                             XmlRpc::XmlRpcValue& result)
 {
+    const vd::VolumeId volName(getID(params[0]));
+
     with_api_exception_conversion([&]()
     {
-        const vd::VolumeId volName(getID(params[0]));
+        XMLRPCStatisticsV2 results_stats;
         const vd::MetaDataStoreStats mdStats(api::getMetaDataStoreStats(volName));
 
+        results_stats.sco_cache_hits = api::getCacheHits(volName);
+        results_stats.sco_cache_misses = api::getCacheMisses(volName);
+        results_stats.cluster_cache_hits = api::getClusterCacheHits(volName);
+        results_stats.cluster_cache_misses = api::getClusterCacheMisses(volName);
+        results_stats.metadata_store_hits = mdStats.cache_hits;
+        results_stats.metadata_store_misses = mdStats.cache_misses;
+        results_stats.stored = api::getStored(volName);
+
+        vd::PerformanceCounters& perf_counters = api::performance_counters(volName);
+        results_stats.performance_counters = perf_counters.v1();
+
+        if (params[0].hasMember(XMLRPCKeys::reset))
+        {
+            const bool reset = getboolWithDefault(params[0],
+                                                  XMLRPCKeys::reset,
+                                                  false);
+            if (reset)
+            {
+                perf_counters.reset_all_counters();
+            }
+        }
+
+        result = XMLRPCStructsBinary::serialize_to_xmlrpc_value(results_stats);
+    });
+}
+
+void
+VolumePerformanceCountersV3::execute_internal(XmlRpc::XmlRpcValue& params,
+                                              XmlRpc::XmlRpcValue& result)
+{
+    const vd::VolumeId volName(getID(params[0]));
+
+    with_api_exception_conversion([&]()
+    {
         XMLRPCStatistics results_stats;
+        const vd::MetaDataStoreStats mdStats(api::getMetaDataStoreStats(volName));
 
         results_stats.sco_cache_hits = api::getCacheHits(volName);
         results_stats.sco_cache_misses = api::getCacheMisses(volName);
@@ -1106,8 +1143,8 @@ VolumePerformanceCounters::execute_internal(XmlRpc::XmlRpcValue& params,
         results_stats.stored = api::getStored(volName);
 
         const be::PartialReadCounter prc(api::getPartialReadCounter(volName));
-        results_stats.partial_read_fast = prc.fast;
         results_stats.partial_read_slow = prc.slow;
+        results_stats.partial_read_slow = prc.fast;
 
         vd::PerformanceCounters& perf_counters = api::performance_counters(volName);
         results_stats.performance_counters = perf_counters;
@@ -1123,13 +1160,59 @@ VolumePerformanceCounters::execute_internal(XmlRpc::XmlRpcValue& params,
             }
         }
 
-        result = XMLRPCStructs::serialize_to_xmlrpc_value(results_stats);
+        result = XMLRPCStructsXML::serialize_to_xmlrpc_value(results_stats);
     });
 }
 
 void
 VolumeDriverPerformanceCounters::execute_internal(XmlRpc::XmlRpcValue& params,
                                                   XmlRpc::XmlRpcValue& result)
+{
+    bool reset = false;
+
+    if (params[0].hasMember(XMLRPCKeys::reset))
+    {
+        reset = getboolWithDefault(params[0],
+                                   XMLRPCKeys::reset,
+                                   false);
+    }
+
+    XMLRPCStatisticsV2 stats;
+    vd::PerformanceCounters perf_counters;
+
+    std::list<vd::VolumeId> l;
+    api::getVolumeList(l);
+
+
+    for (const auto& v : l)
+    {
+        const vd::MetaDataStoreStats mds(api::getMetaDataStoreStats(v));
+        stats.sco_cache_hits += api::getCacheHits(v);
+        stats.sco_cache_misses += api::getCacheMisses(v);
+        stats.cluster_cache_hits += api::getClusterCacheHits(v);
+        stats.cluster_cache_misses += api::getClusterCacheMisses(v);
+        stats.metadata_store_hits += mds.cache_hits;
+        stats.metadata_store_misses += mds.cache_misses;
+        stats.stored += api::getStored(v);
+
+        vd::PerformanceCounters& pc = api::performance_counters(v);
+
+        perf_counters += pc;
+
+        if (reset)
+        {
+            pc.reset_all_counters();
+        }
+    }
+
+    stats.performance_counters = perf_counters.v1();
+
+    result = XMLRPCStructsBinary::serialize_to_xmlrpc_value(stats);
+}
+
+void
+VolumeDriverPerformanceCountersV3::execute_internal(XmlRpc::XmlRpcValue& params,
+                                                    XmlRpc::XmlRpcValue& result)
 {
     bool reset = false;
 
@@ -1158,8 +1241,11 @@ VolumeDriverPerformanceCounters::execute_internal(XmlRpc::XmlRpcValue& params,
         stats.metadata_store_misses += mds.cache_misses;
         stats.stored += api::getStored(v);
 
-        vd::PerformanceCounters& pc = api::performance_counters(v);
+        const be::PartialReadCounter prc(api::getPartialReadCounter(v));
+        stats.partial_read_fast += prc.fast;
+        stats.partial_read_slow += prc.slow;
 
+        vd::PerformanceCounters& pc = api::performance_counters(v);
         perf_counters += pc;
 
         if (reset)
@@ -1170,7 +1256,7 @@ VolumeDriverPerformanceCounters::execute_internal(XmlRpc::XmlRpcValue& params,
 
     stats.performance_counters = perf_counters;
 
-    result = XMLRPCStructs::serialize_to_xmlrpc_value(stats);
+    result = XMLRPCStructsXML::serialize_to_xmlrpc_value(stats);
 }
 
 void
@@ -1210,7 +1296,7 @@ void
 GetNodesStatusMap::execute_internal(XmlRpc::XmlRpcValue& /*params*/,
                             XmlRpc::XmlRpcValue& result)
 {
-    result = XMLRPCStructs::serialize_to_xmlrpc_value(fs_.object_router().get_node_status_map());
+    result = XMLRPCStructsBinary::serialize_to_xmlrpc_value(fs_.object_router().get_node_status_map());
 }
 
 void
@@ -1480,7 +1566,7 @@ UpdateConfiguration::execute_internal(XmlRpc::XmlRpcValue& params,
                          yt::ConfigurationReport>
         rep(api::updateConfiguration((*yt::ConfigFetcher::create(config))(VerifyConfig::T)));
 
-    result = XMLRPCStructs::serialize_to_xmlrpc_value(rep);
+    result = XMLRPCStructsBinary::serialize_to_xmlrpc_value(rep);
 }
 
 void
@@ -1643,7 +1729,7 @@ GetClusterCacheHandleInfo::execute_internal(XmlRpc::XmlRpcValue& params,
                                              info.max_entries,
                                              info.map_stats);
 
-    result = XMLRPCStructs::serialize_to_xmlrpc_value(xinfo);
+    result = XMLRPCStructsBinary::serialize_to_xmlrpc_value(xinfo);
 }
 
 void
@@ -1900,7 +1986,7 @@ ListClientConnections::execute_internal(::XmlRpc::XmlRpcValue& /*params*/,
     int k = 0;
     for (const auto& i: fs_.list_registered_clients())
     {
-        result[k++] = XMLRPCStructs::serialize_to_xmlrpc_value(i);
+        result[k++] = XMLRPCStructsBinary::serialize_to_xmlrpc_value(i);
     }
 }
 

commit fdb11b832f3f196251cb58f016cf5ac361833b22

filesystem: fix deserialization of XMLRPCStructs
Fixes a regression introduced by
 4d10e967 "Restore backward compatibility of PerformanceCounters"
.

https://github.com/openvstorage/volumedriver/issues/264

src/filesystem/XMLRPCStructs.h
@@ -142,7 +142,7 @@ struct XMLRPCSerializer
     void
     set_from_str(const std::string& str)
     {
-        std::stringstream ss;
+        std::stringstream ss(str);
         typename T::iarchive_type ia(ss);
         ia & boost::serialization::make_nvp(T::serialization_name,
                                             static_cast<T&>(*this));

src/filesystem/test/Makefile.am
@@ -85,7 +85,8 @@ volumedriver_fs_test_SOURCES = \
 	StatsCollectorTest.cpp \
 	VolumeTest.cpp \
 	ZMQTest.cpp \
-	ZWorkerPoolTest.cpp
+	ZWorkerPoolTest.cpp \
+	XMLRPCStructsTest.cpp
 
 nodist_volumedriver_fs_test_SOURCES = \
 	ProtobufTest.pb.cc \

src/filesystem/test/XMLRPCStructsTest.cpp
@@ -1,0 +1,53 @@
+// Copyright (C) 2017 iNuron NV
+//
+// This file is part of Open vStorage Open Source Edition (OSE),
+// as available from
+//
+//      http://www.openvstorage.org and
+//      http://www.openvstorage.com.
+//
+// This file is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Affero General Public License v3 (GNU AGPLv3)
+// as published by the Free Software Foundation, in version 3 as it comes in
+// the LICENSE.txt file of the Open vStorage OSE distribution.
+// Open vStorage is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY of any kind.
+
+#include "../XMLRPCStructs.h"
+
+#include <gtest/gtest.h>
+
+namespace volumedriverfstest
+{
+
+using namespace volumedriverfs;
+
+struct XMLRPCStructsTest
+    : public testing::Test
+{
+    template<typename T>
+    static void
+    test_serialization(const T& t)
+    {
+        std::string s(t.str());
+        T u;
+        u.set_from_str(s);
+        EXPECT_EQ(t, u);
+    }
+};
+
+// https://github.com/openvstorage/volumedriver/issues/264
+// overlaps with filesystem-python-client/test/fs_python_client_test.py
+// but this one makes debugging more convenient.
+TEST_F(XMLRPCStructsTest, snapshot_info_serialization)
+{
+    const XMLRPCSnapshotInfo sinfo("foo",
+                                   "bar",
+                                   "baz",
+                                   12345,
+                                   true,
+                                   "");
+    test_serialization(sinfo);
+}
+
+}