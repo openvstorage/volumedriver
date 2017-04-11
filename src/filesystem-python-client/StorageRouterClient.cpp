openvstorage/volumedriver

commit b73c2df93b76f0d22cf589b67c70b4ae93a5e922

(Python) DSOs: initialize -lgcrypt
Addresses https://github.com/openvstorage/volumedriver/issues/309

src/filesystem-python-client/StorageRouterClient.cpp
@@ -55,6 +55,7 @@
 #include <boost/archive/text_oarchive.hpp>
 
 #include <youtils/DimensionedValue.h>
+#include <youtils/Gcrypt.h>
 #include <youtils/Logger.h>
 #include <youtils/LoggerToolCut.h>
 #include <youtils/LoggingToolCut.h>
@@ -325,6 +326,8 @@ BOOST_PYTHON_MODULE(storagerouterclient)
     REGISTER_EXCEPTION_TRANSLATOR(SnapshotNameAlreadyExistsException);
     REGISTER_EXCEPTION_TRANSLATOR(VolumeRestartInProgressException);
     REGISTER_EXCEPTION_TRANSLATOR(VolumeHaltedException);
+
+    yt::Gcrypt::init_gcrypt();
 
     REGISTER_STRINGY_CONVERTER(vfs::ObjectId);
     REGISTER_STRINGY_CONVERTER(vd::VolumeId);
@@ -1462,6 +1465,7 @@ BOOST_PYTHON_MODULE(storagerouterclient)
     scrubbing::python::Scrubber::registerize();
 
     vfspy::ObjectRegistryClient::registerize();
+    vfspy::ScrubManagerClient::registerize();
 
     export_debug_module();
 }

src/backend-python/Backend.cpp
@@ -23,6 +23,7 @@
 #include <boost/python/enum.hpp>
 #include <boost/python/module.hpp>
 
+#include <youtils/Gcrypt.h>
 #include <youtils/Logger.h>
 
 #include <youtils/PythonLogging.h>
@@ -74,6 +75,8 @@ BOOST_PYTHON_MODULE(Backend)
 #undef EXN
 
     youtils::Logger::disableLogging();
+    yt::Gcrypt::init_gcrypt();
+
     using youtils::Severity;
 
     enum_<Severity>("Severity",

src/ganesha-filesystem/Main.cpp
@@ -21,6 +21,8 @@
 #include "Common.h"
 #include "TCPServer.h"
 
+#include <youtils/Gcrypt.h>
+
 extern "C" {
 #include <config_parsing.h>
 #include <FSAL/fsal_config.h>
@@ -372,6 +374,18 @@ static const char OVS_name[] = "OVS";
 MODULE_INIT void
 init(void)
 {
+    try
+    {
+        youtils::Gcrypt::init_gcrypt();
+    }
+    catch (std::exception& e)
+    {
+        LogCrit(COMPONENT_FSAL,
+                const_cast<char*>("Failed to initialize gcrypt: %s"),
+                e.what());
+        return;
+    }
+
     struct fsal_module *myself = &OVS.fsal;
 
     if (register_fsal(myself, OVS_name,

src/volumedriver/ToolCut/ToolCut.cpp
@@ -42,6 +42,7 @@
 #include <boost/python/manage_new_object.hpp>
 #include <boost/python/module.hpp>
 
+#include <youtils/Gcrypt.h>
 #include <youtils/Logger.h>
 #include <youtils/LoggerToolCut.h>
 #include <youtils/LoggingToolCut.h>
@@ -58,6 +59,7 @@ BOOST_PYTHON_MODULE(ToolCut)
     using namespace toolcut;
 
     youtils::Logger::disableLogging();
+    youtils::Gcrypt::init_gcrypt();
 
     MAKE_PYTHON_VD_BOOLEAN_ENUM(OverwriteObject, "Whether to overwrite an existing object in the backend, values are T and F")
 

src/volumedriver/python_scrubber/ScrubberModule.cpp
@@ -19,6 +19,7 @@
 #include <boost/python/enum.hpp>
 #include <boost/python/module.hpp>
 
+#include <youtils/Gcrypt.h>
 #include <youtils/LoggerToolCut.h>
 #include <youtils/LoggingToolCut.h>
 #include <youtils/PythonBuildInfo.h>
@@ -28,6 +29,7 @@ BOOST_PYTHON_MODULE(scrubber)
     youtils::Logger::disableLogging();
 #include <youtils/LoggerToolCut.incl>
 
+    youtils::Gcrypt::init_gcrypt();
     youtils::python::BuildInfo::registerize();
 
     scrubbing::python::Scrubber::registerize();

commit 12e51f900f3e8cf9fd0ec1e682826eb739c91bd4

Python API: fix double registration of ScrubManagerClient
Regression introduced by b73c2df93b76f0d22cf589b67c70b4ae93a5e922

src/filesystem-python-client/StorageRouterClient.cpp
@@ -1465,7 +1465,6 @@ BOOST_PYTHON_MODULE(storagerouterclient)
     scrubbing::python::Scrubber::registerize();
 
     vfspy::ObjectRegistryClient::registerize();
-    vfspy::ScrubManagerClient::registerize();
 
     export_debug_module();
 }