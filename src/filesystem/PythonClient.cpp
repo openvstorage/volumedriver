openvstorage/volumedriver

commit e9c95594b017ef8a8a7fd76e614494b18f991e61

filesystem: suppress compiler warning in code generated by omniorb
... by moving it into a convenience lib and suppressing the warning
about unused variables.

src/filesystem/Makefile.am
@@ -17,12 +17,24 @@ OMNIIDL=@omniidl@
 ShmIdlInterface.cpp : ShmIdlInterface.idl
 	$(OMNIIDL) -bcxx -Wbh=.h -Wbs=.cpp -Wbtp $<
 
+# this convenience lib is a pretty convoluted way of
+# shutting up this warning (g++ 5.4, Ubuntu 16.04):
+# '_0RL_library_version' defined but not used [-Wunused-variable]
+noinst_LTLIBRARIES = \
+	libshmidlinterface.la
+
+libshmidlinterface_a_CXXFLAGS = \
+	$(BUILDTOOLS_CFLAGS) -D_FILE_OFFSET_BITS=64 -Wno-unused-variables
+
+libshmidlinterface_la_SOURCES = ShmIdlInterface.cpp
+
 libvolumedriverfs_la_CXXFLAGS = \
 	$(BUILDTOOLS_CFLAGS) -D_FILE_OFFSET_BITS=64 \
 	-I@abs_top_srcdir@/../msgpack-c/include
 libvolumedriverfs_la_CFLAGS = $(BUILDTOOLS_CFLAGS) -D_FILE_OFFSET_BITS=64
 libvolumedriverfs_la_CPPFLAGS = -I@abs_top_srcdir@/.. -I@abs_top_builddir@/../volumedriver
 libvolumedriverfs_la_LDFLAGS = -static
+libvolumedriverfs_la_LIBADD = libshmidlinterface.la
 
 # this path trickery is necessary as protobuf imports need to start at the same root
 %.pb.cc %.pb.h %_pb2.py: %.proto
@@ -99,7 +111,6 @@ libvolumedriverfs_la_SOURCES = \
 		RemoteNode.cpp \
 		ScrubManager.cpp \
 		ScrubTreeBuilder.cpp \
-		ShmIdlInterface.cpp \
 		ShmInterface.cpp \
 		ShmOrbInterface.cpp \
 		ShmServer.cpp \

src/filesystem/c-api/Makefile.am
@@ -14,7 +14,6 @@ libovsvolumedriver_LTLIBRARIES = libovsvolumedriver.la
 
 libovsvolumedriver_la_SOURCES = \
 	AioCompletion.cpp \
-	../ShmIdlInterface.cpp \
 	ShmControlChannelClient.cpp \
 	ShmContext.cpp \
 	ShmClient.cpp \
@@ -32,7 +31,9 @@ libovsvolumedriver_la_CXXFLAGS = ${BUILDTOOLS_CFLAGS} \
 	-DBOOST_DATE_TIME_POSIX_TIME_STD_CONFIG \
 	-I@abs_top_srcdir@/../msgpack-c/include
 
-libovsvolumedriver_la_LIBADD = ../../youtils/libyoutils.la \
+libovsvolumedriver_la_LIBADD = \
+	../libshmidlinterface.la \
+	../../youtils/libyoutils.la \
 	${BUILDTOOLS_LIBS} \
 	-lxio -lgcc
 

commit f79596c700be7b3e81ef8bfd2c5360856481948d

filesystem: fix breakage introduced by e9c95594b01

src/fawltyfs/corba-daemon/Makefile.am
@@ -26,7 +26,7 @@ libfawlty_sk_a_SOURCES = \
 	fawltySK.cc
 
 libfawlty_sk_a_CXXFLAGS = \
-	$(BUILDTOOLS_CFLAGS) -Wno-unused-variables
+	$(BUILDTOOLS_CFLAGS) -Wno-unused-variable
 
 corba_server_SOURCES = \
 	corba_server.cpp \

src/filesystem/Makefile.am
@@ -23,8 +23,8 @@ ShmIdlInterface.cpp : ShmIdlInterface.idl
 noinst_LTLIBRARIES = \
 	libshmidlinterface.la
 
-libshmidlinterface_a_CXXFLAGS = \
-	$(BUILDTOOLS_CFLAGS) -D_FILE_OFFSET_BITS=64 -Wno-unused-variables
+libshmidlinterface_la_CXXFLAGS = \
+	$(BUILDTOOLS_CFLAGS) -D_FILE_OFFSET_BITS=64 -Wno-unused-variable
 
 libshmidlinterface_la_SOURCES = ShmIdlInterface.cpp
 
@@ -40,6 +40,15 @@ libvolumedriverfs_la_LIBADD = libshmidlinterface.la
 %.pb.cc %.pb.h %_pb2.py: %.proto
 	$(PROTOC) -I$(abs_top_srcdir) -I$(abs_top_srcdir)/../volumedriver --cpp_out=$(builddir) $^
 	$(PROTOC) -I$(abs_top_srcdir) -I$(abs_top_srcdir)/../volumedriver --python_out=$(builddir) $^
+
+BUILT_SOURCES = \
+	FileSystemEvents.pb.cc \
+	FileSystemEvents.pb.h \
+	Messages.pb.h \
+	ShmIdlInterface.cpp \
+	ShmIdlInterface.h \
+	TracePoints_tp.c \
+	TracePoints_tp.h
 
 volumedriverfsdir=@prefix@/share/volumedriverfs
 volumedriverfs_DATA = \
@@ -128,13 +137,6 @@ volumedriverfs_pythondir = @prefix@/lib/python@PYTHON_VERSION@/dist-packages/vol
 
 volumedriverfs_python_PYTHON = \
 	$(abs_builddir)/FileSystemEvents_pb2.py
-
-BUILT_SOURCES = \
-	FileSystemEvents.pb.h \
-	FileSystemEvents_pb2.py \
-	Messages.pb.h \
-	TracePoints_tp.h \
-	TracePoints_tp.c
 
 bin_PROGRAMS = volumedriver_fs
 

src/filesystem/PythonClient.cpp
@@ -121,7 +121,7 @@ PythonClient::redirected_xmlrpc(const std::string& addr,
 #if defined(__GNUC__) &&                        \
     (__GNUC__ <= 4 ||                           \
      (__GNUC__ == 5 && GNUC_MINOR__ <= 4))
-    PRAGMA_IGNORE_WARNING_END("-Wmaybe-uninitialized");
+    PRAGMA_IGNORE_WARNING_END;
 #endif
 
     if (not res)

src/volumedriver/VolManager.cpp
@@ -493,7 +493,7 @@ VolManager::getCurrentVolumesTLogRequirements()
 #if defined(__GNUC__) &&                        \
     (__GNUC__ <= 4 ||                           \
      (__GNUC__ == 5 && GNUC_MINOR__ <= 4))
-        PRAGMA_IGNORE_WARNING_END("-Wmaybe-uninitialized");
+        PRAGMA_IGNORE_WARNING_END
 #endif
     }
 